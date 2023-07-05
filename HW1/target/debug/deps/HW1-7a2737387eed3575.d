9667871c270a35fe8ac4c6900f119ba324a6ea44
Bkisa <bugrahan.kisa@gmail.com>
2023-07-05 17:10:57 +0300

    calculator

---
+++ HW1/target/debug/deps/HW1-7a2737387eed3575.d
@@ -0,0 +1,5 @@
+/home/brhn/workbase_rust/Git/Rust_Sandbox/HW1/target/debug/deps/HW1-7a2737387eed3575.rmeta: src/main.rs
+
+/home/brhn/workbase_rust/Git/Rust_Sandbox/HW1/target/debug/deps/HW1-7a2737387eed3575.d: src/main.rs
+
+src/main.rs:
---
+++ HW1/target/debug/deps/HW1-a1b46c90f9861c85.d
@@ -0,0 +1,5 @@
+/home/brhn/workbase_rust/Git/Rust_Sandbox/HW1/target/debug/deps/HW1-a1b46c90f9861c85.rmeta: src/main.rs
+
+/home/brhn/workbase_rust/Git/Rust_Sandbox/HW1/target/debug/deps/HW1-a1b46c90f9861c85.d: src/main.rs
+
+src/main.rs:
